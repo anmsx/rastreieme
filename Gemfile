ff2a5c96109cdfdb63646335fa26ce9cee638d9d
Eziel Silva <silva.eziel@gmail.com>
2012-02-24 01:37:13 -0200

    Inicio do projeto

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'sqlite3'
+gem 'json'
+gem 'jquery-rails'
+gem 'devise'
+gem 'mongoid'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rastreieme::Application.config.secret_token = 'ddd0f8a10858268b578129394f1c5d8612a0152e2a5c99bc8527b7c7b941050a4f3ad0413ecbe4cb0a57fcb8eaaf4507adfe6c4c50e6b8c27bc01789abdbd7e6'
